a0735e70431bc7e65d067e073e35e78072b32d72
Qohat <qohatpp@gmail.com>
2021-12-10 20:35:05 -0500

    Creating sql script

--- subscriptions.sql
+++ subscriptions.sql
@@ -1,3 +1,19 @@
-CREATE TABLE IF NOT EXISTS users (user_id UUID, slack_user_id varchar, slack_channel_id varchar);
-CREATE TABLE IF NOT EXISTS repositories (repository_id UUID, owner varchar, repository varchar);
-CREATE TABLE IF NOT EXISTS subscriptions (user_id UUID, repository_id UUID, TBD varchar);
+CREATE TABLE IF NOT EXISTS users (
+   user_id VARCHAR UNIQUE NOT NULL,
+   slack_user_id VARCHAR NOT NULL, 
+   slack_channel_id VARCHAR NOT NULL,
+   PRIMARY KEY (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS repositories (
+   repository_id VARCHAR UNIQUE NOT NULL,
+   owner VARCHAR NOT NULL, 
+   repository VARCHAR NOT NULL,
+   PRIMARY KEY (repository_id)
+);
+
+CREATE TABLE IF NOT EXISTS subscriptions (
+   user_id VARCHAR NOT NULL,
+   repository_id VARCHAR NOT NULL, 
+   TBD VARCHAR NOT NULL
+);
